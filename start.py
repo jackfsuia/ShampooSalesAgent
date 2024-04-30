adb64df9c65a16979b9568cb4871a2063daffab2
jackfsuia <53132029+jackfsuia@users.noreply.github.com>
2024-05-01 03:33:11 +0800

    Update start.py

--- start.py
+++ start.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
             elif MODEL.startswith("ernie"):
                 raise Exception("ernie needs API Key and Secret Key. You're missing one of them.(文心一言需要API Key 和 Secret Key, 您少输入了一个。)")
             print(f'You are using {MODEL}\'s API.(你在调用{MODEL}的API。)')
-        elif len(sys.argv) == 4 and (MODEL.startswith("ernie") or MODEL.startswith("wenxin")):
+        elif len(sys.argv) == 4 and (MODEL.startswith("ernie") or MODEL.startswith("ERNIE")):
             from models import ernie_web
             selling_talk = ernie_web.ernie_salesAgent(MODEL, API_Key=sys.argv[2], Secret_Key=sys.argv[3]).closuer_selling_talk()
             print(f'You are using {MODEL}\'s API.(你在调用{MODEL}的API。)')
@@ -34,7 +34,7 @@ if __name__ == '__main__':
             selling_talk = local_model_web.local_salesAgent(MODEL).closuer_selling_talk()
             print(f'You are using your local model {MODEL}.(你在用本地模型{MODEL}。)')
         else:
-            raise Exception("Too many arguments.(参数太多。)")
+            raise Exception("No such model.(此模型不存在。)")
 
         print('Please go to one of the following  links to chat, and the second link can be shared. If the link crash during chat, please keep clicking the Retry button located at the bottom of the chat page. if you need it to speak English, you can just say \'speak english\' to it.(请点击以下任一网址进入聊天，第二个网址可以分享给其他人。聊天的时候崩溃请点聊天框下面的Retry按钮)')
         gr.ChatInterface(selling_talk).launch(share=True)
