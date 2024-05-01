95780521a1e35581e9977e833c274e761f2a4352
jackfsuia <53132029+jackfsuia@users.noreply.github.com>
2024-05-01 11:39:26 +0800

    Update ernie_web.py

--- models/ernie_web.py
+++ models/ernie_web.py
@@ -3,24 +3,6 @@ import re
 import json
 from chat_flow.normal_flow import salesAgent
 
-def get_access_token():
-    """
-    使用 API Key，Secret Key 获取access_token，替换下列示例中的应用API Key、应用Secret Key
-    """
-        
-    url = "https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=[应用API Key]&client_secret=[应用Secret Key]"
-    
-    payload = json.dumps("")
-    headers = {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    }
-    
-    response = requests.request("POST", url, headers=headers, data=payload)
-    return response.json().get("access_token")
-
-
-
 
 class ernie_salesAgent(salesAgent):
 
