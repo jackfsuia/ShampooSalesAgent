3c21cc06170e84134efbc482d29028342db04b21
jackfsuia <53132029+jackfsuia@users.noreply.github.com>
2024-05-03 14:16:25 +0800

    Update gpt_web.py

--- models/gpt_web.py
+++ models/gpt_web.py
@@ -29,7 +29,7 @@ class gpt_salesAgent(salesAgent):
 
     
     def correct_response(self,response):
-        content = response.choices[0].delta.content
+        content = response.choices[0].message.content
         return content
     
   
